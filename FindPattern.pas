ded0b0ff685ea13d227596b1dc4798214bd40d66
Andrii Makarenko <103589486+andmakarenko@users.noreply.github.com>
2024-12-01 21:26:26 +0100

    Update FindPattern.pas

--- FindPattern.pas
+++ FindPattern.pas
@@ -1,84 +1,155 @@
-program RandomFrequencySequence;
+program TimeLoopFinder;
 
-uses sysutils;
+uses
+  SysUtils, Classes;
 
-const
-  n = 1000;
+type
+  TStringMap = record
+    Keys: array of string;
+    Values: array of Integer;
+  end;
 
+procedure AddToMap(var map: TStringMap; key: string; value: Integer);
 var
-  numbers: array[1..n] of integer;
-  frequency: array[1..9] of integer;
-  sequences: array[1..n-2] of string;
-  sequenceCounts: array[1..n-2] of integer;
-  i, j, count: integer;
-  sequence: string;
-  found: boolean;
-
-procedure GenerateRandomNumbers;
+  i: Integer;
 begin
-  Randomize;
-  for i := 1 to n do
-    numbers[i] := Random(9) + 1;  // Random number between 1 and 9
+  for i := 0 to High(map.Keys) do
+  begin
+    if map.Keys[i] = key then
+    begin
+      map.Values[i] := map.Values[i] + value;
+      Exit;
+    end;
+  end;
+  SetLength(map.Keys, Length(map.Keys) + 1);
+  SetLength(map.Values, Length(map.Values) + 1);
+  map.Keys[High(map.Keys)] := key;
+  map.Values[High(map.Values)] := value;
 end;
 
-procedure CalculateFrequency;
-begin
-  for i := 1 to 9 do
-    frequency[i] := 0;
-
-  for i := 1 to n do
-    Inc(frequency[numbers[i]]);
-end;
-
-procedure FindRepeatedSequences;
+procedure RemoveIfLessThanTwo(var map: TStringMap);
+var
+  i, count: Integer;
 begin
   count := 0;
-  for i := 1 to n - 2 do
+  for i := 0 to High(map.Keys) do
   begin
-    sequence := IntToStr(numbers[i]) + ',' + IntToStr(numbers[i + 1]) + ',' + IntToStr(numbers[i + 2]);
-    found := False;
-
-    for j := 1 to count do
+    if map.Values[i] >= 2 then
     begin
-      if sequences[j] = sequence then
+      map.Keys[count] := map.Keys[i];
+      map.Values[count] := map.Values[i];
+      Inc(count);
+    end;
+  end;
+  SetLength(map.Keys, count);
+  SetLength(map.Values, count);
+end;
+
+function GenerateRandomList(n: Integer): TList;
+var
+  i: Integer;
+  intPtr: ^Integer;
+  randomList: TList;
+begin
+  Randomize;
+  randomList := TList.Create;
+  for i := 0 to n - 1 do
+  begin
+    New(intPtr);
+    intPtr^ := Random(10);
+    randomList.Add(intPtr);
+  end;
+  GenerateRandomList := randomList;
+end;
+
+function FindTimeLoops(nums: TList; len: Integer): TStringMap;
+var
+  i, j: Integer;
+  pattern: string;
+  timeLoops: TStringMap;
+begin
+  SetLength(timeLoops.Keys, 0);
+  SetLength(timeLoops.Values, 0);
+
+  for i := 0 to nums.Count - len do
+  begin
+    pattern := '';
+    for j := i to i + len - 1 do
+      pattern := pattern + IntToStr(Integer(PInteger(nums[j])^)) + ',';
+
+    AddToMap(timeLoops, pattern, 1);
+  end;
+
+  RemoveIfLessThanTwo(timeLoops);
+  FindTimeLoops := timeLoops;
+end;
+
+procedure LookForOutliers(timeLoops: TStringMap);
+var
+  avg: Double;
+  max, i: Integer;
+  maxKey: string;
+begin
+  avg := 0.0;
+  max := 0;
+  maxKey := '';
+
+  WriteLn;
+  WriteLn('Gefundene Zeitschleifen in timeLoops:');
+  if Length(timeLoops.Keys) = 0 then
+    WriteLn('Keine Zeitschleifen gefunden.')
+  else
+  begin
+    for i := 0 to High(timeLoops.Keys) do
+    begin
+      WriteLn('Muster: ', timeLoops.Keys[i], ' | Wiederholungen: ', timeLoops.Values[i]);
+      avg := avg + timeLoops.Values[i];
+      if timeLoops.Values[i] > max then
       begin
-        Inc(sequenceCounts[j]);
-        found := True;
-        Break;
+        max := timeLoops.Values[i];
+        maxKey := timeLoops.Keys[i];
       end;
     end;
-
-    if not found then
+    avg := avg / Length(timeLoops.Keys);
+    if max - avg > 3 then
     begin
-      Inc(count);
-      sequences[count] := sequence;
-      sequenceCounts[count] := 1;
+      WriteLn;
+      WriteLn('Vermeintliche Zeitschleife gefunden!');
+      WriteLn(maxKey, ' wurde ', max, ' mal wiederholt.');
+    end
+    else
+    begin
+      WriteLn;
+      WriteLn('Keine vermeintliche Zeitschleife gefunden!');
     end;
   end;
 end;
 
-procedure PrintFrequency;
+procedure FreeRandomList(nums: TList);
+var
+  i: Integer;
 begin
-  Writeln('Frequency of each number:');
-  for i := 1 to 9 do
-    Writeln(i, ': ', frequency[i], ' times');
+  for i := 0 to nums.Count - 1 do
+    Dispose(PInteger(nums[i]));
+  nums.Free;
 end;
 
-procedure PrintRepeatedSequences;
-begin
-  Writeln;  
-  Writeln('Repeated sequences of length 3 that come up more than 4 times:');
-  for i := 1 to count do
-  begin
-    if sequenceCounts[i] > 4 then
-      Writeln(sequences[i], ' appears ', sequenceCounts[i], ' times');
-  end;
-end;
+var
+  randomNums: TList;
+  len: Integer;
+  randomTimeLoops: TStringMap;
 
 begin
-  GenerateRandomNumbers;
-  CalculateFrequency;
-  PrintFrequency;
-  FindRepeatedSequences;
-  PrintRepeatedSequences;
+  // 1. Zufällige nums generieren
+  randomNums := GenerateRandomList(1000);
+
+  // 2. Zeitschleifen erkennen
+  len := 5;
+  randomTimeLoops := FindTimeLoops(randomNums, len);
+
+  // 3. Ergebnisse anzeigen
+  LookForOutliers(randomTimeLoops);
+
+  // 4. Speicher freigeben
+  FreeRandomList(randomNums);
 end.
